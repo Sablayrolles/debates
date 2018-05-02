ca9544d869c5ee13e08dc24364deae277f23ab73
SABLAYROLLES <louis.sablayrolles@gmail.com>
2018-05-02 18:31:39 +0200

    Adding file to use standford corenlp library (server not here needing to install in local) ==> needing to test if it's working (pb of memory)

--- step_2 (M1)/run_server.bat
+++ step_2 (M1)/run_server.bat
@@ -1,5 +1,5 @@
 @echo off
 
 cd C:\Users\louis\Desktop\stage\step_2 (M1)\corenlp
-java -mx1g -cp "*" edu.stanford.nlp.pipeline.StanfordCoreNLPServer -annotators "tokenize,ssplit,pos,lemma,parse,sentiment" -port 9000 -timeout 30000
+java -mx1405m -cp "*" edu.stanford.nlp.pipeline.StanfordCoreNLPServer -annotators "tokenize,ssplit,pos,lemma,parse,sentiment" -port 9000 -timeout 30000
 pause
