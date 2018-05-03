f589a86526d94dccf3f78b56c110c1c5889e13bf
SABLAYROLLES Louis <louis.sablayrolles@gmail.com>
2018-05-03 16:01:27 +0200

    removing oldest testing files

--- step_2 (M1)/run_server.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-
-cd C:\Users\louis\Desktop\stage\step_2 (M1)\corenlp
-java -mx1405m -cp "*" edu.stanford.nlp.pipeline.StanfordCoreNLPServer -annotators "tokenize,ssplit,pos,lemma,parse,sentiment" -port 9000 -timeout 30000
-pause
