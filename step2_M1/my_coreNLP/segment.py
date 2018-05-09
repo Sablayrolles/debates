8994fc589c3bb0856292b9524d161215eb67bba6
SABLAYROLLES <louis.sablayrolles@gmail.com>
2018-05-09 08:18:26 +0200

    Adding save of data struct given by corenlp parse in a file. \n Adding segment sub-module of corenlp\n

---
+++ step2_M1/my_coreNLP/segment.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+
+# Module my_coreNLP/segment
+# Author : SABLAYROLLES Louis
+# Date : 07 / 05 / 17
+
+# own class to segment parsing of corenlp
+
+import pickle
+
+"""
+	Module segment
+	===============
+	
+	This module can be use to segment parsing of corenlp.
+	
+"""
