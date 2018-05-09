b9941535643c77b543c4bbaac0ad58824ee7e2fc
SABLAYROLLES <louis.sablayrolles@gmail.com>
2018-05-09 15:47:11 +0200

    Adding requirements to project

---
+++ generate_requirements.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+# Generator of requirements file
+# Author : SABLAYROLLES Louis
+# Date : 09 / 05 / 17
+
+# generate the requirements.txt file
+# use pip install -r requirements.txt to install all dependancy
+
+pip freeze >requirements.txt
