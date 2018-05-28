2ed8bed3a930ac4210a8ac721aff910bf189ce95
SABLAYROLLES <louis.sablayrolles@gmail.com>
2018-05-28 15:17:41 +0200

    Adding file to compute rpf

---
+++ step2_M1/tools/compute.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+
+# Module tools/compute
+# Author : SABLAYROLLES Louis
+# Date : 28 / 05 / 17
+
+# computre rpf over 3 category
+
+import RappelPrecisionFMesure as rpf
+
+rpfSentences = rpf.FScores(["small", "medium", "long"])
+rpfPunct = rpf.FScores(["small", "medium", "long"])
+
+for i in ["small", "medium", "long"]:
+	continu = " "
+	while continu != "":
+		for j in ["Sentences", "Punct"]:
+			print(i,j)
+			if j == "Sentences":
+				rpfSentences.saisieScore(i)
+			else:
+				rpfPunct.saisieScore(i)
+		print("Another? (blank to stop)")
+		continu = input()
+		
+for i in ["small", "medium", "long"]:
+	for j in ["Sentences", "Punct"]:
+		print("-------------\n",i,j)
+		if j == "Sentences":
+			print("p:",rpfSentences.getMoyPrecision(i))
+			print("r:",rpfSentences.getMoyRappel(i))
+		else:
+			print("p:",rpfPunct.getMoyPrecision(i))
+			print("r:",rpfPunct.getMoyRappel(i))
