6075bd7a442cc66ebba6224ded8d529d1cb61cf8
SABLAYROLLES <louis.sablayrolles@gmail.com>
2018-05-28 09:44:03 +0200

    Adding doc RappelPrecisionFMesure

--- step2_M1/tools/RappelPrecisionFMesure.py
+++ step2_M1/tools/RappelPrecisionFMesure.py
@@ -18,17 +18,66 @@ import pprint
 """
 
 class FScores:
-	def __init__(self, type):
-		self.type = type
+	"""
+		Class FScores
+		=============
+		
+		Use it to manage F-Scores and measures depending
+		
+		def __init__(self, types) : init F-Scores over all types contains in types tabular
+		def addScore(self, type, nbPreditsBonSegments, nbPreditFauxSegments, nbNonPreditsBonSegments) : add a new F-score with all the given parameters to the type type
+		def saisieScore(self, type) : allow people to get the scores on the keyboard and store them
+		def getPrecisions(self, type) : return all the precisions for a given type
+		def getMoyPrecision(self, type) : return the mean of precision for a given type
+		def getRappels(self, type) : return all the recall for a given type
+		def getMoyRappel(self, type) : return the mean of recall for a given type
+		def getFMesures(self, type) : return all the F-measures for a given type
+		def getMoyFMesure(self, type) : return the mean of F-measures for a given type
+	"""
+	def __init__(self, types):
+		"""
+			def __init__(self, types)
+			-------------------------
+			
+			init F-Scores over all types contains in types tabular
+			
+			:param types: all types to add for the measures
+			:type types: string list
+		"""
+		self.types = types
 		
 		self.fscore = {}
-		for t in self.type:
+		for t in self.types:
 			self.fscore[t] = []
 			
 	def addScore(self, type, nbPreditsBonSegments, nbPreditFauxSegments, nbNonPreditsBonSegments):
+		"""
+			def addScore(self, type, nbPreditsBonSegments, nbPreditFauxSegments, nbNonPreditsBonSegments)
+			---------------------------------------------------------------------------------------------
+			
+			add a new F-score with all the given parameters to the type type
+			
+			:param type: category for adding the score
+			:param nbPreditsBonSegments: number of good segment predicts
+			:param nbPreditFauxSegments: number of segment predicts false
+			:param nbNonPreditsBonSegments: number of good segment unpredicted
+			:type type: string
+			:type nbPreditsBonSegments: int
+			:type nbPreditFauxSegments: int
+			:type nbNonPreditsBonSegments: int
+		"""
 		self.fscore[type].append({'VP':nbPreditsBonSegments, 'FP': nbPreditFauxSegments, 'FN':nbNonPreditsBonSegments})
 		
 	def saisieScore(self, type):
+		"""
+			def saisieScore(self, type)
+			---------------------------
+			
+			allow people to get the scores on the keyboard and store them
+			
+			:param type: type of the score to get
+			:type type: string list
+		"""
 		nbPreditsBonSegments = input("Nombre de bon segments prédits : ")
 		nbPreditFauxSegments = input("Nombre de mauvais segments prédits : ")
 		nbNonPreditsBonSegments = input("Nombre de bon segments non prédits : ")
@@ -36,6 +85,17 @@ class FScores:
 		self.addScore(type, nbPreditsBonSegments, nbPreditFauxSegments, nbNonPreditsBonSegments)
 		
 	def getPrecisions(self, type):
+		"""
+			def getPrecisions(self, type)
+			-----------------------------
+			
+			return all the precisions for a given type
+			
+			:param type: category for the precision
+			:type type: string
+			:return: list of all precision for a given type
+			:rtype: float list
+		"""
 		p = []
 		for i in self.fscore[type]:
 			p.append(float(i['VP'])/(float(i['VP']) + float(i['FP'])))
@@ -43,9 +103,31 @@ class FScores:
 		return p
 		
 	def getMoyPrecision(self, type):
+		"""
+			def getMoyPrecision(self, type)
+			-------------------------------
+			
+			return the mean of precision for a given type
+			
+			:param type: category for the precision
+			:type type: string
+			:return: mean of all precision for a given type
+			:rtype: float
+		"""
 		return sum(self.getPrecisions(type)) / (float(len(self.getPrecisions(type))))
 		
 	def getRappels(self, type):
+		"""
+			def getRappels(self, type)
+			--------------------------
+			
+			return all the recall for a given type
+			
+			:param type: category for the recall
+			:type type: string
+			:return: list of all recall for a given type
+			:rtype: float list
+		"""
 		r = []
 		for i in self.fscore[type]:
 			r.append(float(i['VP'])/(float(i['VP']) + float(i['FN'])))
@@ -53,9 +135,31 @@ class FScores:
 		return r
 		
 	def getMoyRappel(self, type):
+		"""
+			def getMoyRappel(self, type)
+			----------------------------
+			
+			return the mean of recall for a given type
+			
+			:param type: category for the recall
+			:type type: string
+			:return: mean of all recall for a given type
+			:rtype: float
+		"""
 		return sum(self.getRappels(type)) / (float(len(self.getRappels(type))))
 		
 	def getFMesures(self, type):
+		"""
+			def getFMesures(self, type)
+			---------------------------
+			
+			return all the F-measures for a given type
+			
+			:param type: category for the F-measures
+			:type type: string
+			:return: list of all F-measures for a given type
+			:rtype: float list
+		"""
 		f = []
 		precisions = self.getPrecisions(type)
 		rappels = self.getRappels(type)
@@ -66,4 +170,15 @@ class FScores:
 		return f
 		
 	def getMoyFMesure(self, type):
+		"""
+			def getMoyFMesure(self, type)
+			-----------------------------
+			
+			return the mean of F-measures for a given type
+			
+			:param type: category for the F-measures
+			:type type: string
+			:return: mean of all F-measures for a given type
+			:rtype: float
+		"""
 		return sum(self.getFMesures(type)) / (float(len(self.getFMesures(type))))
