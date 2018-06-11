32557fec1c42a031a9749a01e3e33bb5d1f1b877
SABLAYROLLES <louis.sablayrolles@gmail.com>
2018-06-11 13:56:30 +0200

    Add learning for logistic softmax

---
+++ step2_M1/learning/logistic_reg.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+
+# Module features/wordFeatures.py
+# Author : SABLAYROLLES Louis
+# Date : 11 / 06 / 17
+
+# learn over features
+
+import sklearn.linear_model as linear_model
+
+#constantes for leaning
+NB_CORE = 4
+MAX_ITER = 100
+
+model = linear_model.LogisticRegression(solver='saga', max_iter=MAX_ITER, multi_class='ovr', n_jobs=NB_CORE)
+#multi_class = 'ovr' ==> regression binaire sur chaque label /='multinomial' sinon
+#solver = For multiclass problems, only ‘newton-cg’, ‘sag’, ‘saga’ and ‘lbfgs’
+
+model.fit(features_train, target_train)
+model.score(features_valid, target_valid)
