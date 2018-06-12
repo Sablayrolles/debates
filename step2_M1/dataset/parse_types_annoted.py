2cbbac07a4cb27fcdb886026ec068d0ad8bf529d
SABLAYROLLES <louis.sablayrolles@gmail.com>
2018-06-12 10:31:43 +0200

    Adding file to get target values in dataset

---
+++ step2_M1/dataset/parse_types_annoted.py
@@ -0,0 +1,70 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+
+# Module dataset/test-parse-xml
+# Author : SABLAYROLLES Louis
+# Date : 12 / 06 / 17
+
+# comptage de chaque type d'EDU
+
+import xml.etree.ElementTree as ET
+import re
+
+def getTypes1stdebate(repertory, number, entete_to_split="([0-9]+ : [A-Z]+ : )"):
+	"""
+		def getTypes1stdebate(repertory, number, entete_to_split="(^[A-Z]+: )")
+		----------------------------------------------------------------------
+		
+		return all types for the an annoted file .aa and .ac
+		:param repertory: repertory of .aa and .ac files
+		:param number: number of question in the debate
+		:param entete_to_split: split entete emitter
+		:type repertory: string
+		:type number: int
+		:type entete_to_split: string
+		:return: dictionnary of types indexed by (numfile, start, stop)
+		:rtype: dictionnary
+	"""
+	
+	types = {}
+	for i in range(1,number+1):
+		print("Parsing q",i)
+		file = repertory+str(i)+"-hand_parsed.aa"
+		f = open(repertory+str(i)+"-hand_parsed.ac")
+		sentences = f.readline()
+		f.close()
+		
+		tree = ET.parse(file)
+		root = tree.getroot()
+		for child in root:
+			if child.tag == "unit":
+				# print("n1:",child.tag)
+				for subchild in child.iterfind("./"):
+					# print("\tn2:",subchild.tag)
+					if subchild.tag == "characterisation":
+						for subsubchild in subchild.iterfind("./"):
+							# print("\t\tn3:",subsubchild.tag)
+							if subsubchild.tag == "type":
+								type = subsubchild.text
+					if subchild.tag == "positioning":
+						for subsubchild in subchild.iterfind("./"):
+							# print("\t\tn3:",subsubchild.tag)
+							if subsubchild.tag == "start":
+								for subsubsubchild in subsubchild.iterfind("./"):
+									# print("\t\t\tn4:",subsubsubchild.tag)
+									start = subsubsubchild.get("index")
+							if subsubchild.tag == "end":
+								for subsubsubchild in subsubchild.iterfind("./"):
+									# print("\t\t\tn4:",subsubsubchild.tag)
+									stop = subsubsubchild.get("index")
+				# print("type:", type, "start:", start, "stop:", stop)
+				if type.lower() not in ['paragraph', 'turn', 'segment', 'default']:
+					txt = sentences[int(start):int(stop)]
+					# print(re.sub(entete_to_split, '', txt))
+					types[(i, re.sub(entete_to_split, '', txt))] = type
+	return types
+
+if __name__ == "__main__":
+	data = getTypes1stdebate("./usa/2016/1/output/ac-aa/", 9)
+	print(data)
+	print("	nbTT:",len(data.keys()))
---
+++ step2_M1/dataset/test-parse-xml.py
@@ -0,0 +1,44 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+
+# Module dataset/test-parse-xml
+# Author : SABLAYROLLES Louis
+# Date : 12 / 06 / 17
+
+# comptage de chaque type d'EDU
+
+import xml.etree.ElementTree as ET
+
+types = {}
+for i in [1,2,3,4,5,6,7,8,9]:
+	file = "C:\\Users\\louis\\Documents\\GitHub\\debates\\step2_M1\\dataset\\usa\\2016\\1\\output\\ac-aa\\"+str(i)+"-hand_parsed.aa"
+	
+	tree = ET.parse(file)
+	root = tree.getroot()
+	for child in root:
+		if child.tag == "unit":
+			print("n1:",child.tag)
+			for subchild in child.iterfind("./"):
+				print("\tn2:",subchild.tag)
+				if subchild.tag == "characterisation":
+					for subsubchild in subchild.iterfind("./"):
+						print("\t\tn3:",subsubchild.tag)
+						if subsubchild.tag == "type":
+							type = subsubchild.text
+				if subchild.tag == "positioning":
+					for subsubchild in subchild.iterfind("./"):
+						print("\t\tn3:",subsubchild.tag)
+						if subsubchild.tag == "start":
+							for subsubsubchild in subsubchild.iterfind("./"):
+								print("\t\t\tn4:",subsubsubchild.tag)
+								start = subsubsubchild.get("index")
+						if subsubchild.tag == "end":
+							for subsubsubchild in subsubchild.iterfind("./"):
+								print("\t\t\tn4:",subsubsubchild.tag)
+								stop = subsubsubchild.get("index")
+			print("type:", type, "start:", start, "stop:", stop)
+			if type.lower() not in ['paragraph', 'dialogue', 'turn', 'segment', 'default']:
+				types[(i, start, stop)] = type
+
+print("types:", types)				
+	
