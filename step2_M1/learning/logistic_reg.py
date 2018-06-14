e9ec5fb4de282b5eed0009f532045b87363f6166
SABLAYROLLES <louis.sablayrolles@gmail.com>
2018-06-14 09:40:16 +0200

    Add learning classes

--- step2_M1/learning/logistic_reg.py
+++ step2_M1/learning/logistic_reg.py
@@ -22,7 +22,7 @@ NB_CORE = 16
 MAX_ITER_MIN = 100
 MAX_ITER_MAX = 1000
 TEST_PERCENT = 0.33
-VERBOSE = False
+VERBOSE = "min"
 
 print("[Param] NB_CORE :", NB_CORE)
 print("[Param] MAX_ITER_MIN :", MAX_ITER_MIN)
@@ -30,7 +30,7 @@ print("[Param] MAX_ITER_MAX :", MAX_ITER_MAX)
 print("[Param] TEST_PERCENT :", TEST_PERCENT)
 print("[Param] VERBOSE :", VERBOSE)
 
-#recupération des données
+### recupération des données
 features = []
 f_dic = []
 print("[Info] Loading features...")
@@ -82,6 +82,7 @@ print("[Data] Targets ToDetermine classifier : ", toDetType)
 print("[Data] Targets Types classifier : ", typesType)
 a = input("Press Enter to Continue ...")
 
+### PRE PROCESSING
 print("[Info] Preprocessing...")
 #on transform le nom des classes en nombre
 le_to_det = preprocess.LabelEncoder()
@@ -96,12 +97,14 @@ print("[Info] Number examples (To determine):", len(featuresToDet))
 print("[Info] Number examples (Classes):", len(featuresTypes))
 a = input("Press Enter to Continue ...")	
 
-print("[Info] Learning To determine")
+### LEARNING TO DETERMINE
+print("[Info] Learning To determine...")
 iter_max = 0
 max_scr = 0
 for MAX_ITER in range(MAX_ITER_MIN,MAX_ITER_MAX):
-	print("Learning test :", float(MAX_ITER) / float(MAX_ITER_MAX-MAX_ITER_MIN) * 100.0,"%")
-	if VERBOSE:
+	if VERBOSE == "min":
+		print("Learning test :", round(float(MAX_ITER) / float(MAX_ITER_MAX-MAX_ITER_MIN) * 100.0, 2),"%")
+	if VERBOSE == "full":
 		print("================= NB ITER :", MAX_ITER, "======================================")
 	#on split le dataset
 	features_train, features_valid, target_train, target_valid = modelSelect.train_test_split(featuresToDet, targetsToDet_trans, test_size=TEST_PERCENT)
@@ -110,52 +113,108 @@ for MAX_ITER in range(MAX_ITER_MIN,MAX_ITER_MAX):
 	#multi_class = 'ovr' ==> regression binaire sur chaque label /='multinomial' sinon
 	#solver = For multiclass problems, only ‘newton-cg’, ‘sag’, ‘saga’ and ‘lbfgs’
 
-	if VERBOSE:
+	if VERBOSE == "full":
 		print("[Info][Model=Todetermine][MAX_ITER="+str(MAX_ITER)+"] Learning...")
 	model = model.fit(features_train, target_train)
-	if VERBOSE:
-		print("[Info][Model=Todetermine][MAX_ITER="+str(MAX_ITER)+"]Testing")
+	if VERBOSE == "full":
+		print("[Info][Model=Todetermine][MAX_ITER="+str(MAX_ITER)+"] Testing")
 
-	if VERBOSE:
-		print("[Info][Model=Todetermine][MAX_ITER="+str(MAX_ITER)+"]Mean train accuracy:",model.score(features_train, target_train))
-		print("[Info][Model=Todetermine][MAX_ITER="+str(MAX_ITER)+"]Mean valid accuracy:",model.score(features_valid, target_valid))
+	if VERBOSE == "full":
+		print("[Info][Model=Todetermine][MAX_ITER="+str(MAX_ITER)+"] Mean train accuracy:",model.score(features_train, target_train))
+		print("[Info][Model=Todetermine][MAX_ITER="+str(MAX_ITER)+"] Mean valid accuracy:",model.score(features_valid, target_valid))
 	v = model.score(features_valid, target_valid)
 	if v > max_scr:
 		max_scr = v
 		iter_max = MAX_ITER
-	if VERBOSE:
-		print("[Info][Model=Todetermine][MAX_ITER="+str(MAX_ITER)+"]Mean valid accuracy:",v)
+	if VERBOSE == "full":
+		print("[Info][Model=Todetermine][MAX_ITER="+str(MAX_ITER)+"] Mean valid accuracy:",v)
 	
-print("[Info][Model=Todetermine][MAX_ITER="+str(MAX_ITER)+"]Best accuracy for", iter_max, "iteration with valid accuracy of", max_scr)
+print("[Info][Model=Todetermine] Best accuracy for", iter_max, "iteration with valid accuracy of", max_scr)
 a = input("Press Enter to Continue ...")
 
-"""
 MAX_ITER = iter_max
-print("================= NB ITER :", MAX_ITER, "======================================")
-features_train, features_valid, target_train, target_valid = modelSelect.train_test_split(features, targets_trans, test_size=0.33)
+print("[Valid] ================= NB ITER :", MAX_ITER, "======================================")
+features_train, features_valid, target_train, target_valid = modelSelect.train_test_split(featuresToDet, targetsToDet_trans, test_size=TEST_PERCENT)
 
 model = linear_model.LogisticRegression(solver='sag', max_iter=MAX_ITER, multi_class='multinomial', n_jobs=NB_CORE)
 #multi_class = 'ovr' ==> regression binaire sur chaque label /='multinomial' sinon
 #solver = For multiclass problems, only ‘newton-cg’, ‘sag’, ‘saga’ and ‘lbfgs’
 
-print("Learning...")
+print("[Valid] Learning...")
 model = model.fit(features_train, target_train)
-print("Testing")
+print("[Valid] Testing")
 
-print("Mean train accuracy:",model.score(features_train, target_train))
-print("Mean valid accuracy:",model.score(features_valid, target_valid))
-print("Types:", le.inverse_transform(model.classes_))
-print("weights:", model.coef_)
-
+print("[Valid] Mean train accuracy:",model.score(features_train, target_train))
+print("[Valid] Mean valid accuracy:",model.score(features_valid, target_valid))
+print("[Valid] Types:", le_to_det.inverse_transform(model.classes_))
+print("[Valid] weights:", model.coef_)
 
 y_pred = model.predict(features_valid)
-y_pred_all = model.predict(features)
-print("y_pred:", le.inverse_transform(y_pred))
-print("y_target:", le.inverse_transform(target_valid))
+y_pred_all = model.predict(featuresToDet)
 
 print("------------------------------")
-print("On valid test")
-print(metrics.classification_report(target_valid, y_pred, target_names=le.classes_))
-print("On all corpus")
-print(metrics.classification_report(targets_trans, y_pred_all, target_names=le.classes_))
-"""
+print("[Valid] On valid test")
+print(metrics.classification_report(target_valid, y_pred, target_names=le_to_det.classes_))
+print("[Valid] On all corpus")
+print(metrics.classification_report(targetsToDet_trans, y_pred_all, target_names=le_to_det.classes_))
+
+### LEARNING CLASSES
+print("[Info] Learning Classes...")
+iter_max = 0
+max_scr = 0
+for MAX_ITER in range(MAX_ITER_MIN,MAX_ITER_MAX):
+	if VERBOSE == "min":
+		print("Learning test :", round(float(MAX_ITER) / float(MAX_ITER_MAX-MAX_ITER_MIN) * 100.0, 2),"%")
+	if VERBOSE == "full":
+		print("================= NB ITER :", MAX_ITER, "======================================")
+	#on split le dataset
+	features_train, features_valid, target_train, target_valid = modelSelect.train_test_split(featuresTypes, targetsTypes_trans, test_size=TEST_PERCENT)
+
+	model = linear_model.LogisticRegression(solver='sag', max_iter=MAX_ITER, multi_class='multinomial', n_jobs=NB_CORE)
+	#multi_class = 'ovr' ==> regression binaire sur chaque label /='multinomial' sinon
+	#solver = For multiclass problems, only ‘newton-cg’, ‘sag’, ‘saga’ and ‘lbfgs’
+
+	if VERBOSE == "full":
+		print("[Info][Model=Classes][MAX_ITER="+str(MAX_ITER)+"] Learning...")
+	model = model.fit(features_train, target_train)
+	if VERBOSE == "full":
+		print("[Info][Model=Classes][MAX_ITER="+str(MAX_ITER)+"] Testing")
+
+	if VERBOSE == "full":
+		print("[Info][Model=Classes][MAX_ITER="+str(MAX_ITER)+"] Mean train accuracy:",model.score(features_train, target_train))
+		print("[Info][Model=Classes][MAX_ITER="+str(MAX_ITER)+"] Mean valid accuracy:",model.score(features_valid, target_valid))
+	v = model.score(features_valid, target_valid)
+	if v > max_scr:
+		max_scr = v
+		iter_max = MAX_ITER
+	if VERBOSE == "full":
+		print("[Info][Model=Classes][MAX_ITER="+str(MAX_ITER)+"] Mean valid accuracy:",v)
+	
+print("[Info][Model=Classes] Best accuracy for", iter_max, "iteration with valid accuracy of", max_scr)
+a = input("Press Enter to Continue ...")
+
+MAX_ITER = iter_max
+print("[Valid] ================= NB ITER :", MAX_ITER, "======================================")
+features_train, features_valid, target_train, target_valid = modelSelect.train_test_split(featuresTypes, targetsTypes_trans, test_size=TEST_PERCENT)
+
+model = linear_model.LogisticRegression(solver='sag', max_iter=MAX_ITER, multi_class='multinomial', n_jobs=NB_CORE)
+#multi_class = 'ovr' ==> regression binaire sur chaque label /='multinomial' sinon
+#solver = For multiclass problems, only ‘newton-cg’, ‘sag’, ‘saga’ and ‘lbfgs’
+
+print("[Valid] Learning...")
+model = model.fit(features_train, target_train)
+print("[Valid] Testing")
+
+print("[Valid] Mean train accuracy:",model.score(features_train, target_train))
+print("[Valid] Mean valid accuracy:",model.score(features_valid, target_valid))
+print("[Valid] Types:", le_to_det.inverse_transform(model.classes_))
+print("[Valid] weights:", model.coef_)
+
+y_pred = model.predict(features_valid)
+y_pred_all = model.predict(featuresToDet)
+
+print("------------------------------")
+print("[Valid] On valid test")
+print(metrics.classification_report(target_valid, y_pred, target_names=le_to_det.classes_))
+print("[Valid] On all corpus")
+print(metrics.classification_report(targetsTypes_trans, y_pred_all, target_names=le_to_det.classes_))
