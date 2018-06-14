f402fb33b5e88e941439860e29871a9d64594520
SABLAYROLLES <louis.sablayrolles@gmail.com>
2018-06-14 14:03:15 +0200

    train split stratified

--- step2_M1/learning/logistic_reg.py
+++ step2_M1/learning/logistic_reg.py
@@ -108,7 +108,7 @@ for MAX_ITER in range(MAX_ITER_MIN,MAX_ITER_MAX):
 		print("================= NB ITER :", MAX_ITER, "======================================")
 	#on split le dataset
 	# features_train, features_valid, target_train, target_valid = modelSelect.train_test_split(featuresOthers, targetsToDet_trans, test_size=TEST_PERCENT)
-	sss = modelSelect.StratifiedShuffleSplit(n_splits=1, test_size=TEST_PERCENT)
+	sss = modelSelect.StratifiedShuffleSplit(n_splits=1, test_size=TEST_PERCENT, group=targetsOthers)
 	train_idx, test_idx = list(sss.split(featuresOthers, targetsToDet_trans))[0]
 	features_train, features_valid, target_train, target_valid = [], [], [], []
 	for i, j in zip(train_idx, test_idx):
@@ -143,8 +143,8 @@ print("[Info][Model=Others] Best accuracy for", iter_max, "iteration with valid 
 MAX_ITER = iter_max
 print("[Valid] ================= NB ITER :", MAX_ITER, "======================================")
 # features_train, features_valid, target_train, target_valid = modelSelect.train_test_split(featuresOthers, targetsToDet_trans, test_size=TEST_PERCENT)
-sss = modelSelect.StratifiedShuffleSplit(n_splits=1, test_size=TEST_PERCENT)
-train_idx, test_idx = list(sss.split(featuresOthers, targetsToDet_trans))[0]
+sss = modelSelect.StratifiedShuffleSplit(n_splits=10, test_size=TEST_PERCENT)
+train_idx, test_idx = list(sss.split(featuresOthers, targetsToDet_trans, group=targetsOthers))[0]
 features_train, features_valid, target_train, target_valid = [], [], [], []
 for i, j in zip(train_idx, test_idx):
 	features_train.append(featuresOthers[i])
