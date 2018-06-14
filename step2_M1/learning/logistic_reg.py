f32b21e19eb5b5fa8a9c02a8a6fe43a29dd7d7d2
SABLAYROLLES <louis.sablayrolles@gmail.com>
2018-06-14 11:40:26 +0200

    Better learning.

--- step2_M1/learning/logistic_reg.py
+++ step2_M1/learning/logistic_reg.py
@@ -109,7 +109,7 @@ for MAX_ITER in range(MAX_ITER_MIN,MAX_ITER_MAX):
 	#on split le dataset
 	features_train, features_valid, target_train, target_valid = modelSelect.train_test_split(featuresOthers, targetsToDet_trans, test_size=TEST_PERCENT)
 
-	model = linear_model.LogisticRegression(solver='liblinear', max_iter=MAX_ITER, multi_class='multinomial', n_jobs=NB_CORE)
+	model = linear_model.LogisticRegression(solver='liblinear', max_iter=MAX_ITER, n_jobs=NB_CORE)
 	#multi_class = 'ovr' ==> regression binaire sur chaque label /='multinomial' sinon
 	#solver = For multiclass problems, only ‘newton-cg’, ‘sag’, ‘saga’ and ‘lbfgs’
 
