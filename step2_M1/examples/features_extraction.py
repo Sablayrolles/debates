f7efd64af5d43d5026baa0db10ed8f88461416d4
SABLAYROLLES <louis.sablayrolles@gmail.com>
2018-06-15 10:13:01 +0200

    Add example file for features extraction

---
+++ step2_M1/examples/features_extraction.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+
+import sys
+sys.path.append("..")
+
+import my_coreNLP.parseNLP as parseNLP
+
+import features.saveData as saveData
+import features.computeFeatures as computeFeatures
+
+NLP = parseNLP.StanfordNLP()
+txt = "My cat is eating the mouse!"
+
+s = saveData.compute(txt, NLP)
+f = computeFeatures.returnFeatures(s, ["as?", "as!", "nb1stPers", "nb2ndPers"])
+
+print(f)
