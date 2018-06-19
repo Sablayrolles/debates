862369ceb9eebc2b041a9ff944917b02f42f112b
SABLAYROLLES <louis.sablayrolles@gmail.com>
2018-06-19 11:13:07 +0200

    Update counter nb edu types

--- step2_M1/dataset/__counter_number_type_EDU.py
+++ step2_M1/dataset/__counter_number_type_EDU.py
@@ -14,12 +14,14 @@ types = {}
 
 for num in [1,2,3,4,5,6,7,8,9]:
 	name = file+str(num)+"-hand_parsed.aa"
-	
+	nFiles = 0
+	tFiles = {}
 	f = open(name,"r")
 	for line in f.readlines():			
 		if "<type>" in line: # and line[6:-8][0].isupper():
 			if line[6:-8] not in ["default", "paragraph", "Dialogue", "Turn"]:
 				nbEDU += 1
+				nFiles += 1
 				if line[6:-8] not in types.keys() and line[6:-8] != "Segment":
 					types[line[6:-8]] = 1
 				else:
@@ -30,6 +32,20 @@ for num in [1,2,3,4,5,6,7,8,9]:
 							types["Other"] += 1
 						else:
 							types["Other"] = 1
+							
+				if line[6:-8] not in tFiles.keys() and line[6:-8] != "Segment":
+					tFiles[line[6:-8]] = 1
+				else:
+					if line[6:-8] != "Segment":
+						tFiles[line[6:-8]] += 1
+					else:
+						if "Other" in tFiles.keys():
+							tFiles["Other"] += 1
+						else:
+							tFiles["Other"] = 1
+	print("For file num",num)
+	print("nEDU:", nFiles)
+	print("types:", tFiles)
 				
 
 print("nbEDU:", nbEDU)
