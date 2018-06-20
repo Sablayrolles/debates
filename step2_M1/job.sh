66a7a6dbd6906e5bd85982d0d35ce0720ba31474
SABLAYROLLES <louis.sablayrolles@gmail.com>
2018-06-20 13:35:46 +0200

    update script job.sh

--- step2_M1/job.sh
+++ step2_M1/job.sh
@@ -22,6 +22,11 @@ cd dataset
 python3 parse_types_annoted.py >$home/target.log 2>$home/target.err
 if [ $? -ne 0 ]; then
 	echo -e "Fail parse types" | mailx -v -s "[Error] Fail parse types" -a $home/target.log -a $home/target.err -S smtp-use-starttls -S ssl-verify=ignore -S smtp-auth=login -S smtp=smtp://smtp.gmail.com:587 -S from="louis.sablayrolles@gmail.com(Sablayrolles Louis)" -S smtp-auth-user=louis.sablayrolles@gmail.com -S smtp-auth-password=eragon1996 -S ssl-verify=ignore -S nss-config-dir=~/.certs louis.sablayrolles@gmail.com
+	
+	echo "Kill corenlp"
+	pid=`ps -aux | grep "lsabalyr" | grep "java" | head -1 | awk '{print $2}'`
+	kill -9 $pid
+	
 	exit 2
 fi
 
@@ -30,26 +35,41 @@ echo "nbFiles="$nbFiles
 
 cd ../features
 echo "Extracting infos features"
-echo "python3 saveData.py $nbFiles >>$home/job.log 2>>$home/job.err; echo $?"
+echo "python3 saveData.py $nbFiles >>$home/features.log 2>>$home/features.err; echo $?"
 a=`python3 saveData.py $nbFiles >$home/features.log 2>$home/features.err; echo $?`
 if [ $a -ne 0 ]; then
-	echo -e "Fail saveData" | mailx -v -s "[Error] Fail run corenlp" -a $home/features.log -a $home/features.err -S smtp-use-starttls -S ssl-verify=ignore -S smtp-auth=login -S smtp=smtp://smtp.gmail.com:587 -S from="louis.sablayrolles@gmail.com(Sablayrolles Louis)" -S smtp-auth-user=louis.sablayrolles@gmail.com -S smtp-auth-password=eragon1996 -S ssl-verify=ignore -S nss-config-dir=~/.certs louis.sablayrolles@gmail.com
+	echo -e "Fail saveData" | mailx -v -s "[Error] Fail saveData" -a $home/features.log -a $home/features.err -S smtp-use-starttls -S ssl-verify=ignore -S smtp-auth=login -S smtp=smtp://smtp.gmail.com:587 -S from="louis.sablayrolles@gmail.com(Sablayrolles Louis)" -S smtp-auth-user=louis.sablayrolles@gmail.com -S smtp-auth-password=eragon1996 -S ssl-verify=ignore -S nss-config-dir=~/.certs louis.sablayrolles@gmail.com
+	
+	echo "Kill corenlp"
+	pid=`ps -aux | grep "lsabalyr" | grep "java" | head -1 | awk '{print $2}'`
+	kill -9 $pid
+	
 	exit 3
 fi
 echo "Extracting features"
-echo "python3 computeFeatures.py $nbFiles >>$home/job.log 2>>$home/job.err; echo $?"
+echo "python3 computeFeatures.py $nbFiles >>$home/features.log 2>>$home/features.err; echo $?"
 a=`python3 computeFeatures.py $nbFiles >>$home/features.log 2>>$home/features.err; echo $?`
 if [ $a -ne 0 ]; then
 	echo -e "Fail computeFeatures" | mailx -v -s "[Error] Fail computeFeatures" -a $home/features.log -a $home/features.err -S smtp-use-starttls -S ssl-verify=ignore -S smtp-auth=login -S smtp=smtp://smtp.gmail.com:587 -S from="louis.sablayrolles@gmail.com(Sablayrolles Louis)" -S smtp-auth-user=louis.sablayrolles@gmail.com -S smtp-auth-password=eragon1996 -S ssl-verify=ignore -S nss-config-dir=~/.certs louis.sablayrolles@gmail.com
+	
+	echo "Kill corenlp"
+	pid=`ps -aux | grep "lsabalyr" | grep "java" | head -1 | awk '{print $2}'`
+	kill -9 $pid
+	
 	exit 4
 fi
 
 cd ../learning
 echo "Learning"
-echo "python3 logistic_reg.py $nbFiles >>$home/job.log 2>>$home/job.err; echo $?"
+echo "python3 logistic_reg.py $nbFiles >>$home/learn.log 2>>$home/learn.err; echo $?"
 a=`python3 logistic_reg.py $nbFiles >>$home/learn.log 2>>$home/learn.err; echo $?`
 if [ $a -ne 0 ]; then
 	echo -e "Fail logistic_reg" | mailx -v -s "[Error] Fail logistic_reg" -a $home/learn.log -a $home/learn.err -S smtp-use-starttls -S ssl-verify=ignore -S smtp-auth=login -S smtp=smtp://smtp.gmail.com:587 -S from="louis.sablayrolles@gmail.com(Sablayrolles Louis)" -S smtp-auth-user=louis.sablayrolles@gmail.com -S smtp-auth-password=eragon1996 -S ssl-verify=ignore -S nss-config-dir=~/.certs louis.sablayrolles@gmail.com
+	
+	echo "Kill corenlp"
+	pid=`ps -aux | grep "lsabalyr" | grep "java" | head -1 | awk '{print $2}'`
+	kill -9 $pid
+
 	exit 5
 fi
 
