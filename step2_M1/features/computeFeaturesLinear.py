cee3feafee3daf758e5315ea077e507a52782798
SABLAYROLLES <louis.sablayrolles@gmail.com>
2018-06-27 11:27:31 +0200

    Adding new features to crf and linear features

---
+++ step2_M1/features/computeFeaturesLinear.py
@@ -0,0 +1,234 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+
+# Module features/computeFeatures.py
+# Author : SABLAYROLLES Louis
+# Date : 11 / 06 / 17
+
+# compute features
+
+import joblib
+import sys
+sys.path.append("./")
+sys.path.append("../")
+import argparse
+
+try:
+	from . import wordFeatures as wFeatures
+	from . import tokenFeatures as tFeatures
+	from ..my_emoLex import emoLex as my_emoLex
+except ImportError:
+	import wordFeatures as wFeatures
+	import tokenFeatures as tFeatures
+	import my_emoLex.emoLex as my_emoLex
+except SystemError:
+	import wordFeatures as wFeatures
+	import tokenFeatures as tFeatures
+	import my_emoLex.emoLex as my_emoLex
+	
+"""
+	Module computeFeaturesLinear
+	============================
+	
+	This module can be use to compute and save features that depend of others EDU on a computeFeatures
+	
+"""
+counters = {}
+def returnFeatures(data, preComputeFeatures, NB_PREV, NB_NEXT, featuresList, allNumbered=False):
+	"""
+		def returnFeatures(data, featuresList)
+		--------------------------------------
+		
+		return dictionnary of features for an EDU
+		
+		:param data: data extract from saveData.py
+		:type data: dictionnary
+		:return: dictionnary of features for an EDU
+		:rtype: dictionnary
+	"""
+	features = {}
+	boolean = not(allNumbered)
+	
+	#-------------------------------------------------------------------------------------------------------------------------------
+	#increasing counters
+	global counters
+	#topics
+	if "nbEDUTopics" not in counters.keys() or counters["old_q"] != data[0]["question"]:
+		counters["nbEDUTopics"] = 1
+		counters["old_q"] = data[0]["question"]
+	else:
+		counters["nbEDUTopics"] += 1
+	if "nb"+data[0]["emitter"]+"topic" not in counters.keys() or counters["old_q"] != data[0]["question"]:
+		counter["nb"+data[0]["emitter"]+"topic"] = 1
+	else:
+		counter["nb"+data[0]["emitter"]+"topic"] += 1
+		
+	#segments
+	if "old_speaker" not in counters.keys() or counters["old_q"] != data[0]["question"] or data[0]["emitter"] != counters["old_speaker"]:
+		counters["old_speaker"] = data[0]["emitter"]
+		counters["nbEDUSegment"] = 1
+	else:
+		counter["nbEDUSegment"] += 1
+	#-------------------------------------------------------------------------------------------------------------------------------
+	
+	for f in featuresList:
+		#tokens features	
+		if f == "as?":
+			features["as?"] = tFeatures.asQuestionMark(data[0]["tokens"], boolean)
+		if f == "nbTokSameEDUPrev":
+			for i in range(NB_PREV):
+				if data[-i] != None:
+					features["nbTokSameEDUPrev:"+str(-i)] = tFeatures.nbSameTok(data[0]["tokens"], data[-i]["tokens"])
+				else:
+					features["nbTokSameEDUPrev:"+str(-i)] = 0
+ 		if f == "nbTokSameEDUNext":
+			for i in range(NB_NEXT):
+				if data[i] != None:
+					features["nbTokSameEDUNext:"+str(i)] = tFeatures.nbSameTok(data[0]["tokens"], data[i]["tokens"])
+				else:
+					features["nbTokSameEDUNext:"+str(i)] = 0
+		#env
+		if f == "positionInTopic":
+			features["positionInTopic"] = counters["nbEDUTopics"]
+		if f == "positionInSegment":
+			features["positionInSegment"] = counters["nbEDUSegment"]
+		if f == "nbEDUsaidBySpeakerInTopic":
+			features["nbEDUsaidBySpeakerInTopic"] = counters["nb"+data[0]["emitter"]+"topic"]
+		if f == "nbEDUsaidByOthersSpeakerInTopic":
+			features["nbEDUsaidByOthersSpeakerInTopic"] = 0 
+			for k in counters.keys():
+				if "topic" in k and "nb" in k and data[0]["emitter"] not in k:
+					features["nbEDUsaidByOthersSpeakerInTopic"] += counters[k]
+	
+	for k in preComputeFeatures["0"].keys():
+		features[k] = preComputeFeatures["0"][k]
+	return features
+
+NB_FAITS = 0
+def processEDULogReg(n, nbTT, NB_PREV, NB_NEXT):
+	global NB_FAITS
+	
+	#calcul words
+	data = {}
+	data[0] = joblib.load("./data/"+str(n)+".data")
+	for i in range(n-NB_PREV,n):
+		try:
+			with open("./data/"+str(i)+".data"): pass
+			data[n-i] = joblib.load("./data/"+str(i)+".data")
+		except IOError:
+			data[n-i] = None
+					
+	for i in range(n+1, NB_NEXT+1):
+		try:
+			with open("./data/"+str(i)+".data"): pass
+			data[i-n] = joblib.load("./data/"+str(i)+".data")
+		except IOError:
+			data[i-n] = None
+	
+	features = {}
+	features['0'] = joblib.load("./data/"+str(n)+".features")
+	for i in range(n-NB_PREV,n):
+		try:
+			with open("./data/"+str(i)+".features"): pass
+			features[n-i] = joblib.load("./data/"+str(i)+".features")
+		except IOError:
+			features[n-i] = None
+					
+	for i in range(n+1, NB_NEXT+1):
+		try:
+			with open("./data/"+str(i)+".data"): pass
+			features[i-n] = joblib.load("./data/"+str(i)+".features")
+		except IOError:
+			features[i-n] = None
+			
+	f = returnFeatures(data, features, NB_PREV, NB_NEXT, ["nbTokSameEDUPrev", "nbTokSameEDUNext", "positionInTopic", "positionInSegment", "nbEDUsaidBySpeakerInTopic", "nbEDUsaidBySpeakerInTopic", False)
+	joblib.dump(f,"./data/"+str(f["num"])+".features");
+	
+	NB_FAITS += 1
+	print('\033[1A'+"[Features] Computing features : ",NB_FAITS,"/",nbTT)
+	
+	return 0
+	
+def processEDUCRF(n, nbTT, NB_PREV, NB_NEXT):
+	global NB_FAITS
+	
+	#calcul words
+	data = {}
+	data[0] = joblib.load("./data/"+str(n)+".data")
+	for i in range(n-NB_PREV,n):
+		try:
+			with open("./data/"+str(i)+".data"): pass
+			data[n-i] = joblib.load("./data/"+str(i)+".data")
+		except IOError:
+			data[n-i] = None
+					
+	for i in range(n+1, NB_NEXT+1):
+		try:
+			with open("./data/"+str(i)+".data"): pass
+			data[i-n] = joblib.load("./data/"+str(i)+".data")
+		except IOError:
+			data[i-n] = None
+	
+	features = {}
+	features['0'] = joblib.load("./data/"+str(n)+".features")
+		for i in range(n-NB_PREV,n):
+		try:
+			with open("./data/"+str(i)+".features"): pass
+			features[n-i] = joblib.load("./data/"+str(i)+".features")
+		except IOError:
+			features[n-i] = None
+					
+	for i in range(n+1, NB_NEXT+1):
+		try:
+			with open("./data/"+str(i)+".data"): pass
+			features[n+i] = joblib.load("./data/"+str(i)+".features")
+		except IOError:
+			features[n+i] = None
+			
+	f = returnFeatures(data, features, NB_PREV, NB_NEXT, ["nbTokSameEDUPrev", "nbTokSameEDUNext", "positionInTopic", "positionInSegment", "nbEDUsaidBySpeakerInTopic", "nbEDUsaidBySpeakerInTopic")
+	joblib.dump(f,"./data/"+str(f["num"])+".features");
+	
+	NB_FAITS += 1
+	print('\033[1A'+"[Features] Computing features : ",NB_FAITS,"/",nbTT)
+	
+	return 0
+	
+if __name__ == '__main__':
+	parser = argparse.ArgumentParser(description="\tModule computeFeaturesLinear\n\t===============\n\n\t\tThis module can be use to compute and save features that depend of others EDU on a computeFeatures")
+
+	parser.add_argument("numFiles", metavar="nbFiles", type=int, help="Number of file .data to extract and save features (from saveData)")
+	parser.add_argument("typeLearning", metavar="typeLearning", type=int, choices=[1, 2], help="Type of learning after\n\t1 : regression logistique\n\t 2 : CRF")
+	parser.add_argument("-p", "--previous", metavar="previous", type=int, nargs="?", help="Nb core previous to see (default 2)")
+	parser.add_argument("-n", "--next", metavar="next", type=int, nargs="?", help="Nb core next to see (default 1)")
+
+	args = parser.parse_args()
+
+	nbTT = int(args.numFiles) + 1
+	typeLearn = int(args.typeLearning)
+	if args.previous != None:
+		NB_PREV = int(args.previous)
+	else:
+		NB_PREV = 2
+	if args.next != None:
+		NB_NEXT = int(args.next)
+	else:
+		NB_NEXT = 1
+		
+	print("Param : nbFiles="+str(nbTT))
+	print("Param : typeLearn="+str(typeLearn)+" [1 : regression logistique, 2 : CRF]")
+	print("Param : NB_PREV="+str(NB_PREV))
+	print("Param : NB_NEXT="+str(NB_NEXT))
+		
+	features = joblib.load("./data/1.features")
+	print("Features_already_existing:", features.keys())
+		
+	print("\n")
+	nbTT = int(sys.argv[1])+1
+	
+	for i in range(1,nbTT):
+		if typeLearn == 1:
+			processEDULogReg(i, nbTT, NB_PREV, NB_NEXT)
+		if typeLearn == 2:
+			processEDUCRF(i, nbTT, NB_PREV, NB_NEXT)
+	
+	print("Computing ", int(sys.argv[1])+1, "files")
---
+++ step2_M1/jobCRF.sh
@@ -0,0 +1,167 @@
+#!/bin/sh
+
+echo "\nUsage : $0 -[esv0] [-c NBCORE]"
+echo "-e : Send email when error"
+echo "-s : Send email when success"
+echo "-v : Send email after each step(verbose)"
+echo "-0 : No notification"
+echo -e "default : -ens -c 8\n\n"
+
+notifE=1
+notifS=1
+notifN=1
+NBCORE=8
+
+if [ -n $1 ]; then	
+	if [ `echo $1 | grep "v" | wc -l` -eq 1 ]; then
+		notifN=1
+		notifE=0
+		notifS=0
+	fi;
+	
+	if [ `echo $1 | grep "s" | wc -l` -eq 1 ]; then
+		notifS=1
+		notifN=0
+		notifE=0
+	fi;
+	
+	if [ `echo $1 | grep "e" | wc -l` -eq 1 ]; then
+		notifE=1
+		notifS=0
+		notifN=0
+	fi;
+	
+	if [ `echo $1 | grep "0" | wc -l` -eq 1 ]; then
+		notifE=0
+		notifS=0
+		notifN=0
+	fi;
+	if [ `echo $1 | grep "c" | wc -l` -eq 1 ]; then
+		if [ -n $2 ]; then
+			NBCORE=$2
+		fi;
+	fi;
+fi;
+
+echo -e "NBCORE"$NBCORE"\nNotification error:"$notifE"\nNotification success:"$notifS"\nNotification step:"$notifN
+
+dated=`date "+%y/%m/%d %H:%M:%S"`
+echo "Running java corenlp"
+cd ~/stageM1/corenlp/
+./run.sh >~/stageM1/debates/step2\_M1/corenlp.log 2>~/stageM1/debates/step2\_M1/corenlp.err &
+if [ $? -ne 0 ] && [ $notifE -eq 1 ]; then
+	datef=`date "+%y/%m/%d %H:%M:%S"`
+	echo -e "Fail run corenlp\nBegging on $dated \n Finnishing on $datef\n\n" | mailx -v -s "[Error] Fail run corenlp" -a ~/stageM1/debates/step2\_M1/corenlp.log -a ~/stageM1/debates/step2\_M1/corenlp.err -S smtp-use-starttls -S ssl-verify=ignore -S smtp-auth=login -S smtp=smtp://smtp.gmail.com:587 -S from="louis.sablayrolles@gmail.com(Sablayrolles Louis)" -S smtp-auth-user=louis.sablayrolles@gmail.com -S smtp-auth-password=eragon1996 -S ssl-verify=ignore -S nss-config-dir=~/.certs louis.sablayrolles@gmail.com >/dev/null 2>/dev/null
+	exit 1
+fi	
+
+echo "Extracting EDU"
+cd ~/stageM1/debates/step2_M1/
+rm -f target.err 2>/dev/null
+rm -f target.log 2>/dev/null
+rm -f features.log 2>/dev/null
+rm -f features.err 2>/dev/null
+rm -f job.log 2>/dev/null
+rm -f job.err 2>/dev/null
+home=`pwd`
+cd dataset
+if [ $notifN -eq 1 ]; then
+	datef=`date "+%y/%m/%d %H:%M:%S"`
+	echo -e "" | mailx -v -s "[Info] Start parsing data on $datef" -S smtp-use-starttls -S ssl-verify=ignore -S smtp-auth=login -S smtp=smtp://smtp.gmail.com:587 -S from="louis.sablayrolles@gmail.com(Sablayrolles Louis)" -S smtp-auth-user=louis.sablayrolles@gmail.com -S smtp-auth-password=eragon1996 -S ssl-verify=ignore -S nss-config-dir=~/.certs louis.sablayrolles@gmail.com >/dev/null 2>/dev/null
+fi;
+python3 parse_types_annoted.py >$home/target.log 2>$home/target.err
+if [ $? -ne 0 ] && [ $notifE -eq 1 ]; then
+	datef=`date "+%y/%m/%d %H:%M:%S"`
+	echo -e "Fail parse types\nBegging on $dated \n Finnishing on $datef\n\n" | mailx -v -s "[Error] Fail parse types" -a $home/target.log -a $home/target.err -S smtp-use-starttls -S ssl-verify=ignore -S smtp-auth=login -S smtp=smtp://smtp.gmail.com:587 -S from="louis.sablayrolles@gmail.com(Sablayrolles Louis)" -S smtp-auth-user=louis.sablayrolles@gmail.com -S smtp-auth-password=eragon1996 -S ssl-verify=ignore -S nss-config-dir=~/.certs louis.sablayrolles@gmail.com >/dev/null 2>/dev/null
+	
+	echo "Kill corenlp"
+	pid=`ps -aux | grep "lsabalyr" | grep "java" | head -1 | awk '{print $2}'`
+	kill -9 $pid
+	
+	exit 2
+fi
+
+nbFiles=`cat tmp`
+echo "nbFiles="$nbFiles
+
+cd ../features
+echo "Extracting infos features"
+echo "python3 saveData.py $nbFiles -c $NBCORE >>$home/features.log 2>>$home/features.err; echo \$?"
+if [ $notifN -eq 1 ]; then
+	datef=`date "+%y/%m/%d %H:%M:%S"`
+	echo -e "" | mailx -v -s "[Info] Start extracting feature on $datef" -S smtp-use-starttls -S ssl-verify=ignore -S smtp-auth=login -S smtp=smtp://smtp.gmail.com:587 -S from="louis.sablayrolles@gmail.com(Sablayrolles Louis)" -S smtp-auth-user=louis.sablayrolles@gmail.com -S smtp-auth-password=eragon1996 -S ssl-verify=ignore -S nss-config-dir=~/.certs louis.sablayrolles@gmail.com >/dev/null 2>/dev/null
+fi;
+a=`python3 saveData.py $nbFiles -c $NBCORE >$home/features.log 2>$home/features.err; echo $?`
+if [ $a -ne 0 ] && [ $notifE -eq 1 ]; then
+	datef=`date "+%y/%m/%d %H:%M:%S"`
+	echo -e "Fail saveData\nBegging on $dated \n Finnishing on $datef\n\n" | mailx -v -s "[Error] Fail saveData" -a $home/features.log -a $home/features.err -S smtp-use-starttls -S ssl-verify=ignore -S smtp-auth=login -S smtp=smtp://smtp.gmail.com:587 -S from="louis.sablayrolles@gmail.com(Sablayrolles Louis)" -S smtp-auth-user=louis.sablayrolles@gmail.com -S smtp-auth-password=eragon1996 -S ssl-verify=ignore -S nss-config-dir=~/.certs louis.sablayrolles@gmail.com >/dev/null 2>/dev/null
+	
+	echo "Kill corenlp"
+	pid=`ps -aux | grep "lsabalyr" | grep "java" | head -1 | awk '{print $2}'`
+	kill -9 $pid
+	
+	exit 3
+fi
+echo "Extracting features"
+echo "python3 computeFeatures.py $nbFiles 2 -c $NBCORE >>$home/features.log 2>>$home/features.err; echo \$?"
+if [ $notifN -eq 1 ]; then
+	datef=`date "+%y/%m/%d %H:%M:%S"`
+	echo -e "" | mailx -v -s "[Info] Start compute features on $datef" -S smtp-use-starttls -S ssl-verify=ignore -S smtp-auth=login -S smtp=smtp://smtp.gmail.com:587 -S from="louis.sablayrolles@gmail.com(Sablayrolles Louis)" -S smtp-auth-user=louis.sablayrolles@gmail.com -S smtp-auth-password=eragon1996 -S ssl-verify=ignore -S nss-config-dir=~/.certs louis.sablayrolles@gmail.com >/dev/null 2>/dev/null
+fi;
+a=`python3 computeFeatures.py $nbFiles 2 -c $NBCORE >>$home/features.log 2>>$home/features.err; echo $?`
+if [ $a -ne 0 ] && [ $notifE -eq 1 ]; then
+	datef=`date "+%y/%m/%d %H:%M:%S"`
+	echo -e "Fail computeFeatures\nBegging on $dated \n Finnishing on $datef\n\n" | mailx -v -s "[Error] Fail computeFeatures" -a $home/features.log -a $home/features.err -S smtp-use-starttls -S ssl-verify=ignore -S smtp-auth=login -S smtp=smtp://smtp.gmail.com:587 -S from="louis.sablayrolles@gmail.com(Sablayrolles Louis)" -S smtp-auth-user=louis.sablayrolles@gmail.com -S smtp-auth-password=eragon1996 -S ssl-verify=ignore -S nss-config-dir=~/.certs louis.sablayrolles@gmail.com >/dev/null 2>/dev/null
+	
+	echo "Kill corenlp"
+	pid=`ps -aux | grep "lsabalyr" | grep "java" | head -1 | awk '{print $2}'`
+	kill -9 $pid
+	
+	exit 4
+fi
+echo "python3 computeFeaturesLinear.py $nbFiles 1 -p 2 -n 1 >>$home/features.log 2>>$home/features.err; echo \$?"
+if [ $notifN -eq 1 ]; then
+	datef=`date "+%y/%m/%d %H:%M:%S"`
+	echo -e "" | mailx -v -s "[Info] Start compute features on $datef" -S smtp-use-starttls -S ssl-verify=ignore -S smtp-auth=login -S smtp=smtp://smtp.gmail.com:587 -S from="louis.sablayrolles@gmail.com(Sablayrolles Louis)" -S smtp-auth-user=louis.sablayrolles@gmail.com -S smtp-auth-password=eragon1996 -S ssl-verify=ignore -S nss-config-dir=~/.certs louis.sablayrolles@gmail.com >/dev/null 2>/dev/null
+fi;
+a=`python3 computeFeaturesLinear.py $nbFiles 1 -p 2 -n 1  >>$home/features.log 2>>$home/features.err; echo $?`
+if [ $a -ne 0 ] && [ $notifE -eq 1 ]; then
+	datef=`date "+%y/%m/%d %H:%M:%S"`
+	echo -e "Fail computeFeatures\nBegging on $dated \n Finnishing on $datef\n\n" | mailx -v -s "[Error] Fail computeFeatures" -a $home/features.log -a $home/features.err -S smtp-use-starttls -S ssl-verify=ignore -S smtp-auth=login -S smtp=smtp://smtp.gmail.com:587 -S from="louis.sablayrolles@gmail.com(Sablayrolles Louis)" -S smtp-auth-user=louis.sablayrolles@gmail.com -S smtp-auth-password=eragon1996 -S ssl-verify=ignore -S nss-config-dir=~/.certs louis.sablayrolles@gmail.com >/dev/null 2>/dev/null
+	
+	echo "Kill corenlp"
+	pid=`ps -aux | grep "lsabalyr" | grep "java" | head -1 | awk '{print $2}'`
+	kill -9 $pid
+	
+	exit 4
+fi
+
+cd ../learning
+echo "Learning"
+echo "python3 crf.py $nbFiles -c $NBCORE >>$home/learn.log 2>>$home/learn.err; echo \$?"
+if [ $notifN -eq 1 ]; then
+	datef=`date "+%y/%m/%d %H:%M:%S"`
+	echo -e "" | mailx -v -s "[Info] Start learning on $datef" -S smtp-use-starttls -S ssl-verify=ignore -S smtp-auth=login -S smtp=smtp://smtp.gmail.com:587 -S from="louis.sablayrolles@gmail.com(Sablayrolles Louis)" -S smtp-auth-user=louis.sablayrolles@gmail.com -S smtp-auth-password=eragon1996 -S ssl-verify=ignore -S nss-config-dir=~/.certs louis.sablayrolles@gmail.com >/dev/null 2>/dev/null
+fi;
+a=`python3 crf.py $nbFiles -c $NBCORE >>$home/learn.log 2>>$home/learn.err; echo $?`
+if [ $a -ne 0 ] && [ $notifE -eq 1 ]; then
+	datef=`date "+%y/%m/%d %H:%M:%S"`
+	echo -e "Fail logistic_reg\nBegging on $dated \n Finnishing on $datef\n\n" | mailx -v -s "[Error] Fail crf" -a $home/learn.log -a $home/learn.err -S smtp-use-starttls -S ssl-verify=ignore -S smtp-auth=login -S smtp=smtp://smtp.gmail.com:587 -S from="louis.sablayrolles@gmail.com(Sablayrolles Louis)" -S smtp-auth-user=louis.sablayrolles@gmail.com -S smtp-auth-password=eragon1996 -S ssl-verify=ignore -S nss-config-dir=~/.certs louis.sablayrolles@gmail.com >/dev/null 2>/dev/null
+	
+	echo "Kill corenlp"
+	pid=`ps -aux | grep "lsabalyr" | grep "java" | head -1 | awk '{print $2}'`
+	kill -9 $pid
+
+	exit 5
+fi
+
+echo "Kill corenlp"
+pid=`ps -aux | grep "lsabalyr" | grep "java" | head -1 | awk '{print $2}'`
+kill -9 $pid
+
+#send result by mail
+if [ $notifS -eq 1 ]; then
+	datef=`date "+%y/%m/%d %H:%M:%S"`
+	res=`cat $home/learning/res`
+	echo -e "Result learning finish\nBegging on $dated \n Finnishing on $datef\n\n $res" | mailx -v -s "[Result] Result learning" -a $home/target.log -a $home/target.err -a $home/features.log -a $home/features.err -a $home/learn.log -a $home/learn.err -S smtp-use-starttls -S ssl-verify=ignore -S smtp-auth=login -S smtp=smtp://smtp.gmail.com:587 -S from="louis.sablayrolles@gmail.com(Sablayrolles Louis)" -S smtp-auth-user=louis.sablayrolles@gmail.com -S smtp-auth-password=eragon1996 -S ssl-verify=ignore -S nss-config-dir=~/.certs louis.sablayrolles@gmail.com >/dev/null 2>/dev/null
+fi;
