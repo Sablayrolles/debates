6e02ca1ad07c1e554ad40ffb0be801cbcf5506c6
SABLAYROLLES <louis.sablayrolles@gmail.com>
2018-07-03 17:14:53 +0200

    del old graph and add best result value in legend

--- step2_M1/jobCRF.sh
+++ step2_M1/jobCRF.sh
@@ -152,7 +152,7 @@ if [ $notifN -eq 1 ]; then
 	sendMail louis.sablayrolles@gmail.com "[Info] Start learning on $datef" " "
 fi;
 echo "Setting crontab"
-echo '0,30 * * * * rm ~/stageM1/debates/step2\_M1/learning/graph.png && python3 ~/stageM1/debates/step2\_M1/learning/plot3d.py >/dev/null 2>/dev/null && echo "Plot crf\n" | mailx -v -s "[Info] Plot crf" -a ~/stageM1/debates/step2\_M1/learning/graph.png  -a ~/stageM1/debates/step2\_M1/learnCRF.err -a ~/stageM1/debates/step2\_M1/learnCRF.log -S smtp-use-starttls -S ssl-verify=ignore -S smtp-auth=login -S smtp=smtp://smtp.gmail.com:587 -S from="louis.sablayrolles@gmail.com(Sablayrolles Louis)" -S smtp-auth-user=louis.sablayrolles@gmail.com -S smtp-auth-password=eragon1996 -S ssl-verify=ignore -S nss-config-dir=~/.certs louis.sablayrolles@gmail.com >>~/logs/mailx.log 2>~/logs/mailx.err'>./cron
+echo '0,30 * * * * rm ~/stageM1/debates/step2\_M1/learning/graph.png ; python3 ~/stageM1/debates/step2\_M1/learning/plot3d.py >/dev/null 2>/dev/null ; echo "Plot crf\n" | mailx -v -s "[Info] Plot crf" -a ~/stageM1/debates/step2\_M1/learning/graph.png  -a ~/stageM1/debates/step2\_M1/learnCRF.err -a ~/stageM1/debates/step2\_M1/learnCRF.log -S smtp-use-starttls -S ssl-verify=ignore -S smtp-auth=login -S smtp=smtp://smtp.gmail.com:587 -S from="louis.sablayrolles@gmail.com(Sablayrolles Louis)" -S smtp-auth-user=louis.sablayrolles@gmail.com -S smtp-auth-password=eragon1996 -S ssl-verify=ignore -S nss-config-dir=~/.certs louis.sablayrolles@gmail.com >>~/logs/mailx.log 2>~/logs/mailx.err'>./cron
 crontab ./cron
 rm ./cron
 a=`python3 crf.py $nbFiles -v 0 >>$home/learnCRF.log 2>>$home/learnCRF.err; echo $?`
