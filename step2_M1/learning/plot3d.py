36c3e7c28313ceab045cb34795a9a11e207927c0
SABLAYROLLES <louis.sablayrolles@gmail.com>
2018-07-06 11:07:17 +0200

    update compile graphs

--- step2_M1/learning/plot3d.py
+++ step2_M1/learning/plot3d.py
@@ -54,7 +54,7 @@ purple = mpatches.Patch(color='purple', label='0.95 <= acc')
 last = ""
 max_s = 0
 best = ""
-x = [0.5,0.1,0.15,0.2,0.25,0.3,0.35,0.4,0.45,0.5,0.55,0.6,0.65,0.7,0.75,0.8,0.85,0.9,0.95]
+x = [0.05,0.1,0.15,0.2,0.25,0.3,0.35,0.4,0.45,0.5,0.55,0.6,0.65,0.7,0.75,0.8,0.85,0.9,0.95]
 c1Graph =  {'x': x, 'y': [0 for i in range(len(x))], 'nb': [1 for i in range(len(x))]}
 
 c2Graph =  {'x': x, 'y': [0 for i in range(len(x))], 'nb': [1 for i in range(len(x))]}
