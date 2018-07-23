ae5f0d5fb033dc9a0559fb7cbb3641cb2f27eb71
SABLAYROLLES <louis.sablayrolles@gmail.com>
2018-07-23 11:42:38 +0200

    add parallel loop for crf

--- step2_M1/jobCRF.sh
+++ step2_M1/jobCRF.sh
@@ -3,13 +3,13 @@
 #pour fonction sendMail
 source ~/.bash_profile >/dev/null
 
-echo "\nUsage : $0 -[esv0] [-c NBCORE]"
+echo "  --   Usage : $0 -[esv0] [-c NBCORE]"
 echo "-e : Send email when error"
 echo "-s : Send email when success"
 echo "-v : Send email after each step(verbose)"
 echo "-c : nb core max 7 for features extraction all for learning"
 echo "-0 : No notification"
-echo -e "default : -ens -c 7\n\n"
+echo -e "default : -ens -c 7  --     --   "
 
 notifE=1
 notifS=1
@@ -53,7 +53,7 @@ if [ -n $1 ]; then
 	fi;
 fi;
 
-echo -e "NBCORE:"$NBCORE"\nNBCORElearn:"$NBCORE_l"\nNotification error:"$notifE"\nNotification success:"$notifS"\nNotification step:"$notifN
+echo -e "NBCORE:"$NBCORE"  --   NBCORElearn:"$NBCORE_l"  --   Notification error:"$notifE"  --   Notification success:"$notifS"  --   Notification step:"$notifN
 
 rm -f corenlp.err 2>/dev/null
 rm -f corenlp.log 2>/dev/null
@@ -64,7 +64,7 @@ cd ~/stageM1/corenlp/
 ./run.sh >>~/stageM1/debates/step2\_M1/corenlp.log 2>>~/stageM1/debates/step2\_M1/corenlp.err &
 if [ $? -ne 0 ] && [ $notifE -eq 1 ]; then
 	datef=`date "+%y/%m/%d %H:%M:%S"`
-	sendMail louis.sablayrolles@gmail.com "[Error] Fail run corenlp" "Fail run corenlp\nBegging on $dated \n Finnishing on $datef\n\n" ~/stageM1/debates/step2\_M1/corenlp.log ~/stageM1/debates/step2\_M1/corenlp.err
+	sendMail louis.sablayrolles@gmail.com "[Error] Fail run corenlp" "Fail run corenlp  --   Begging on $dated   --    Finnishing on $datef  --     --   " ~/stageM1/debates/step2\_M1/corenlp.log ~/stageM1/debates/step2\_M1/corenlp.err
 	exit 1
 fi	
 
@@ -81,12 +81,12 @@ cd dataset
 echo "python3 parse_types_annoted.py >$home/targetCRF.log 2>$home/target.err"
 if [ $notifN -eq 1 ]; then
 	datef=`date "+%y/%m/%d %H:%M:%S"`
-	sendMail louis.sablayrolles@gmail.com "[Info] Start parsing data on $datef" " "
+	sendMail louis.sablayrolles@gmail.com "[Info] Start parse_types_annoted on $datef" " "
 fi;
 python3 parse_types_annoted.py >$home/targetCRF.log 2>$home/target.err
 if [ $? -ne 0 ] && [ $notifE -eq 1 ]; then
 	datef=`date "+%y/%m/%d %H:%M:%S"`
-	sendMail louis.sablayrolles@gmail.com "[Error] Fail parse types" "Fail parse types\nBegging on $dated \n Finnishing on $datef\n\n" ~/stageM1/debates/step2\_M1/targetCRF.log ~/stageM1/debates/step2\_M1/targetCRF.err
+	sendMail louis.sablayrolles@gmail.com "[Error] Fail parse_types_annoted" "Fail parse_types_annoted  --   Begging on $dated   --    Finnishing on $datef  --     --   " ~/stageM1/debates/step2\_M1/targetCRF.log ~/stageM1/debates/step2\_M1/targetCRF.err
 	
 	echo "Kill corenlp"
 	pid=`ps -aux | grep "lsabalyr" | grep "java" | head -1 | awk '{print $2}'`
@@ -103,12 +103,12 @@ echo "Extracting infos features"
 echo "python3 saveData.py $nbFiles -c $NBCORE >>$home/featuresCRF.log 2>>$home/featuresCRF.err; echo \$?"
 if [ $notifN -eq 1 ]; then
 	datef=`date "+%y/%m/%d %H:%M:%S"`
-	sendMail louis.sablayrolles@gmail.com "[Info] Start extracting feature on $datef" " " 
+	sendMail louis.sablayrolles@gmail.com "[Info] Start saveData on $datef" " " 
 fi;
 a=`python3 saveData.py $nbFiles -c $NBCORE >$home/featuresCRF.log 2>$home/featuresCRF.err; echo $?`
 if [ $a -ne 0 ] && [ $notifE -eq 1 ]; then
 	datef=`date "+%y/%m/%d %H:%M:%S"`
-	sendMail louis.sablayrolles@gmail.com "[Error] Fail saveData" "Fail saveData\nBegging on $dated \n Finnishing on $datef\n\n" ~/stageM1/debates/step2\_M1/featuresCRF.log ~/stageM1/debates/step2\_M1/featuresCRF.err
+	sendMail louis.sablayrolles@gmail.com "[Error] Fail saveData" "Fail saveData  --   Begging on $dated   --    Finnishing on $datef  --     --   " ~/stageM1/debates/step2\_M1/featuresCRF.log ~/stageM1/debates/step2\_M1/featuresCRF.err
 	
 	echo "Kill corenlp"
 	pid=`ps -aux | grep "lsabalyr" | grep "java" | head -1 | awk '{print $2}'`
@@ -120,12 +120,12 @@ echo "Extracting features"
 echo "python3 computeFeatures.py $nbFiles 2 -c $NBCORE >>$home/featuresCRF.log 2>>$home/featuresCRF.err; echo \$?"
 if [ $notifN -eq 1 ]; then
 	datef=`date "+%y/%m/%d %H:%M:%S"`
-	sendMail louis.sablayrolles@gmail.com "[Info] Start computing feature on $datef" " " 
+	sendMail louis.sablayrolles@gmail.com "[Info] Start computeFeatures on $datef" " " 
 fi;
 a=`python3 computeFeatures.py $nbFiles 2 -c $NBCORE >>$home/featuresCRF.log 2>>$home/featuresCRF.err; echo $?`
 if [ $a -ne 0 ] && [ $notifE -eq 1 ]; then
 	datef=`date "+%y/%m/%d %H:%M:%S"`
-	sendMail louis.sablayrolles@gmail.com "[Error] Fail computeFeatures" "Fail computeFeatures\nBegging on $dated \n Finnishing on $datef\n\n" ~/stageM1/debates/step2\_M1/featuresCRF.log ~/stageM1/debates/step2\_M1/featuresCRF.err
+	sendMail louis.sablayrolles@gmail.com "[Error] Fail computeFeatures" "Fail computeFeatures  --   Begging on $dated   --    Finnishing on $datef  --     --   " ~/stageM1/debates/step2\_M1/featuresCRF.log ~/stageM1/debates/step2\_M1/featuresCRF.err
 	
 	echo "Kill corenlp"
 	pid=`ps -aux | grep "lsabalyr" | grep "java" | head -1 | awk '{print $2}'`
@@ -136,12 +136,12 @@ fi
 echo "python3 computeFeaturesLinear.py $nbFiles 2 -p 2 -n 1 >>$home/featuresCRF.log 2>>$home/featuresCRF.err; echo \$?"
 if [ $notifN -eq 1 ]; then
 	datef=`date "+%y/%m/%d %H:%M:%S"`
-	sendMail louis.sablayrolles@gmail.com "[Info] Start compute linear features on $datef" " "
+	sendMail louis.sablayrolles@gmail.com "[Info] Start computeFeaturesLinear on $datef" " "
 fi;
 a=`python3 computeFeaturesLinear.py $nbFiles 2 -p 2 -n 1  >>$home/featuresCRF.log 2>>$home/featuresCRF.err; echo $?`
 if [ $a -ne 0 ] && [ $notifE -eq 1 ]; then
 	datef=`date "+%y/%m/%d %H:%M:%S"`
-	sendMail louis.sablayrolles@gmail.com "[Error] Fail computeFeaturesLinear" "Fail computeFeaturesLinear\nBegging on $dated \n Finnishing on $datef\n\n" ~/stageM1/debates/step2\_M1/featuresCRF.log ~/stageM1/debates/step2\_M1/featuresCRF.err
+	sendMail louis.sablayrolles@gmail.com "[Error] Fail computeFeaturesLinear" "Fail computeFeaturesLinear  --   Begging on $dated   --    Finnishing on $datef  --     --   " ~/stageM1/debates/step2\_M1/featuresCRF.log ~/stageM1/debates/step2\_M1/featuresCRF.err
 	
 	echo "Kill corenlp"
 	pid=`ps -aux | grep "lsabalyr" | grep "java" | head -1 | awk '{print $2}'`
@@ -160,16 +160,16 @@ echo "python3 crf.py $nbFiles -v 0 -c $NBCORE_l>>$home/learnCRF.log 2>>$home/lea
 echo "File of plotting : scrs"
 if [ $notifN -eq 1 ]; then
 	datef=`date "+%y/%m/%d %H:%M:%S"`
-	sendMail louis.sablayrolles@gmail.com "[Info] Start learning on $datef" " "
+	sendMail louis.sablayrolles@gmail.com "[Info] Start learnCRF on $datef" " "
 fi;
 echo "Setting crontab"
-echo '0 * * * * python3 ~/stageM1/debates/step2\_M1/learning/plot3d.py >/dev/null 2>/dev/null ; echo "Plot crf\n" | mailx -v -s "[Info] Plot crf" -a ~/graph.png -a ~/accuracies.png  -a ~/stageM1/debates/step2\_M1/learnCRF.err -a ~/stageM1/debates/step2\_M1/learnCRF.log -S smtp-use-starttls -S ssl-verify=ignore -S smtp-auth=login -S smtp=smtp://smtp.gmail.com:587 -S from="louis.sablayrolles@gmail.com(Sablayrolles Louis)" -S smtp-auth-user=louis.sablayrolles@gmail.com -S smtp-auth-password=eragon1996 -S ssl-verify=ignore -S nss-config-dir=~/.certs louis.sablayrolles@gmail.com >>~/logs/mailx.log 2>~/logs/mailx.err'>./cron
+echo '0 * * * * python3 ~/stageM1/debates/step2\_M1/learning/plot3d.py >/dev/null 2>/dev/null ; echo "Plot crf  --   " | mailx -v -s "[Info] Plot crf" -a ~/graph.png -a ~/accuracies.png  -a ~/stageM1/debates/step2\_M1/learnCRF.err -a ~/stageM1/debates/step2\_M1/learnCRF.log -S smtp-use-starttls -S ssl-verify=ignore -S smtp-auth=login -S smtp=smtp://smtp.gmail.com:587 -S from="louis.sablayrolles@gmail.com(Sablayrolles Louis)" -S smtp-auth-user=louis.sablayrolles@gmail.com -S smtp-auth-password=eragon1996 -S ssl-verify=ignore -S nss-config-dir=~/.certs louis.sablayrolles@gmail.com >>~/logs/mailx.log 2>~/logs/mailx.err'>./cron
 crontab ./cron
 rm ./cron
 a=`python3 crf_para.py $nbFiles -v 0 -c $NBCORE_l >>$home/learnCRF.log 2>>$home/learnCRF.err; echo $?`
 if [ $a -ne 0 ] && [ $notifE -eq 1 ]; then
 	datef=`date "+%y/%m/%d %H:%M:%S"`
-	sendMail louis.sablayrolles@gmail.com "[Error] Fail crf" "Fail crf\nBegging on $dated \n Finnishing on $datef\n\n" ~/stageM1/debates/step2\_M1/learnCRF.log ~/stageM1/debates/step2\_M1/learnCRF.err
+	sendMail louis.sablayrolles@gmail.com "[Error] Fail learnCRF" "Fail learnCRF  --   Begging on $dated   --    Finnishing on $datef  --     --   " ~/stageM1/debates/step2\_M1/learnCRF.log ~/stageM1/debates/step2\_M1/learnCRF.err
 	
 	echo "Del crontab"
 	crontab -r
@@ -189,5 +189,5 @@ kill -n 9 $pid
 if [ $notifS -eq 1 ]; then
 	datef=`date "+%y/%m/%d %H:%M:%S"`
 	res=`cat $home/learning/res`
-	sendMail louis.sablayrolles@gmail.com "[Result] Result crf" "Result crf finish\nBegging on $dated \n Finnishing on $datef\n\n $res" ~/stageM1/debates/step2\_M1/targetCRF.log ~/stageM1/debates/step2\_M1/targetCRF.err ~/stageM1/debates/step2\_M1/featuresCRF.log ~/stageM1/debates/step2\_M1/featuresCRF.err ~/stageM1/debates/step2\_M1/learnCRF.log ~/stageM1/debates/step2\_M1/learnCRF.err
+	sendMail louis.sablayrolles@gmail.com "[Result] Result crf" "Result crf finish  --   Begging on $dated   --    Finnishing on $datef  --     --    $res" ~/stageM1/debates/step2\_M1/targetCRF.log ~/stageM1/debates/step2\_M1/targetCRF.err ~/stageM1/debates/step2\_M1/featuresCRF.log ~/stageM1/debates/step2\_M1/featuresCRF.err ~/stageM1/debates/step2\_M1/learnCRF.log ~/stageM1/debates/step2\_M1/learnCRF.err
 fi;
